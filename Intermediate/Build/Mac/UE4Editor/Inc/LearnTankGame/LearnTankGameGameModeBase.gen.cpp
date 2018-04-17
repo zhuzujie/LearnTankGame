5c9332679efc17d366c61eeec021098e258d08ca
朱祖杰 <zhuzujie@onemt.com>
2018-04-18 00:14:44 +0800

    init

---
+++ Intermediate/Build/Mac/UE4Editor/Inc/LearnTankGame/LearnTankGameGameModeBase.gen.cpp
@@ -0,0 +1,69 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "LearnTankGameGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeLearnTankGameGameModeBase() {}
+// Cross Module References
+	LEARNTANKGAME_API UClass* Z_Construct_UClass_ALearnTankGameGameModeBase_NoRegister();
+	LEARNTANKGAME_API UClass* Z_Construct_UClass_ALearnTankGameGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_LearnTankGame();
+// End Cross Module References
+	void ALearnTankGameGameModeBase::StaticRegisterNativesALearnTankGameGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_ALearnTankGameGameModeBase_NoRegister()
+	{
+		return ALearnTankGameGameModeBase::StaticClass();
+	}
+	UClass* Z_Construct_UClass_ALearnTankGameGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+				(UObject* (*)())Z_Construct_UPackage__Script_LearnTankGame,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+				{ "IncludePath", "LearnTankGameGameModeBase.h" },
+				{ "ModuleRelativePath", "LearnTankGameGameModeBase.h" },
+				{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+			};
+#endif
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<ALearnTankGameGameModeBase>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&ALearnTankGameGameModeBase::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00900288u,
+				nullptr, 0,
+				nullptr, 0,
+				nullptr,
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ALearnTankGameGameModeBase, 1471046025);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ALearnTankGameGameModeBase(Z_Construct_UClass_ALearnTankGameGameModeBase, &ALearnTankGameGameModeBase::StaticClass, TEXT("/Script/LearnTankGame"), TEXT("ALearnTankGameGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ALearnTankGameGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ Intermediate/Build/Mac/UE4Editor/Inc/LearnTankGame/LearnTankGameGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LEARNTANKGAME_LearnTankGameGameModeBase_generated_h
+#error "LearnTankGameGameModeBase.generated.h already included, missing '#pragma once' in LearnTankGameGameModeBase.h"
+#endif
+#define LEARNTANKGAME_LearnTankGameGameModeBase_generated_h
+
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_RPC_WRAPPERS
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesALearnTankGameGameModeBase(); \
+	friend LEARNTANKGAME_API class UClass* Z_Construct_UClass_ALearnTankGameGameModeBase(); \
+public: \
+	DECLARE_CLASS(ALearnTankGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/LearnTankGame"), NO_API) \
+	DECLARE_SERIALIZER(ALearnTankGameGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesALearnTankGameGameModeBase(); \
+	friend LEARNTANKGAME_API class UClass* Z_Construct_UClass_ALearnTankGameGameModeBase(); \
+public: \
+	DECLARE_CLASS(ALearnTankGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/LearnTankGame"), NO_API) \
+	DECLARE_SERIALIZER(ALearnTankGameGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALearnTankGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALearnTankGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALearnTankGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALearnTankGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALearnTankGameGameModeBase(ALearnTankGameGameModeBase&&); \
+	NO_API ALearnTankGameGameModeBase(const ALearnTankGameGameModeBase&); \
+public:
+
+
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALearnTankGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALearnTankGameGameModeBase(ALearnTankGameGameModeBase&&); \
+	NO_API ALearnTankGameGameModeBase(const ALearnTankGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALearnTankGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALearnTankGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALearnTankGameGameModeBase)
+
+
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_12_PROLOG
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_RPC_WRAPPERS \
+	LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_INCLASS \
+	LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LearnTankGame_Source_LearnTankGame_LearnTankGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
